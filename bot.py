397cd4b6bda573d09a31ddee0cf398229d584af0
MC <cpc1007@gmail.com>
2015-09-21 02:12:30 -0400

    wrote code to connect script to an irc channel and added a few commands to listen for.

--- bot.py
+++ bot.py
@@ -1,7 +1,37 @@
 import subprocess
+import socket
+import time
 
-def test():
+def ircConnect():
+    network = 'irc.freenode.net'
+    port = 6667
+    nick = 'doshBot'
+    channel = '#doshchannel'
+
+    irc = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+    irc.connect( (network, port) )
+    print irc.recv( 4096 )
+    irc.send("USER " + nick + " " + nick + " " + nick + " :This is a test\n")
+    irc.send("NICK " + nick + "\n")
+    irc.send("JOIN " + channel + '\n')
+
+    while 1:
+        text = irc.recv(2040)
+        print text
+        #time.sleep(5)
+        if(text.find('PING') != -1):
+            irc.send('PONG ' + text.split()[1] + '\r\n')
+
+        if(text.find(':!speak') != -1):
+            irc.send('PRIVMSG ' + channel + ' :What up' + '\r\n')
+
+        if(text.find(':!echo') != -1):
+            echo()
+
+
+
+def echo():
     subprocess.call(['echo', 'hello'], shell=True)
 
 if __name__ == '__main__':
-    test()
+    ircConnect()
